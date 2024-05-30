28aa0f0b456a20f474a804a978e140fec2e868ee
anoukvanbogaert <vb.anouk@gmail.com>
2024-05-30 14:51:14 -0700

    added circular progress logic with random time

---
+++ one-day-challenge/src/components/ProgressCircle.vue
@@ -0,0 +1,50 @@
+<template>
+    <svg class="progress-circle" viewBox="0 0 100 100">
+        <circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="#FF66BA"
+            stroke-width="5"
+            fill="transparent"
+            :stroke-dasharray="circumference"
+            :stroke-dashoffset="strokeDashOffset"
+            transform="rotate(-90 50 50)"
+        />
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        totalTime: {
+            type: Number,
+            required: true,
+        },
+        initialTime: {
+            type: Number,
+            required: true,
+        },
+    },
+    computed: {
+        circumference() {
+            return 2 * Math.PI * 45;
+        },
+        strokeDashOffset() {
+            if (this.initialTime === 0) return this.circumference;
+            const progress = 1 - this.initialTime / this.totalTime;
+            return this.circumference * progress;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.progress-circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- one-day-challenge/src/components/TimerView.vue
+++ one-day-challenge/src/components/TimerView.vue
@@ -1,27 +1,93 @@
 <template>
-    <div class="timer-top"><CircleBackGround /></div>
+    <div class="timer-top">
+        <CircleBackGround />
+        <ProgressCircle :totalTime="totalTime" :initialTime="initialTime" />
+    </div>
     <div class="timer-bottom">
-        <div>time</div>
-        <div>back to home button</div>
+        <div class="formatted-time">{{ formattedTime }}</div>
+        <div class="back-home">
+            <v-icon color="accent" icon="mdi-home" @click="goToHome"></v-icon>
+            <div class="back-home-text">back to home</div>
+        </div>
     </div>
 </template>
 <script>
 import CircleBackGround from './CircleBackGround.vue';
+import ProgressCircle from './ProgressCircle.vue';
+
 export default {
     components: {
         CircleBackGround,
+        ProgressCircle,
+    },
+    data() {
+        return {
+            totalTime: this.getRandomTime(),
+            initialTime: 0,
+        };
+    },
+    computed: {
+        formattedTime() {
+            const hours = String(Math.floor(this.totalTime / 3600)).padStart(2, '0');
+            const minutes = String(Math.floor((this.totalTime % 3600) / 60)).padStart(2, '0');
+            const seconds = String(this.totalTime % 60).padStart(2, '0');
+            return `${hours}:${minutes}:${seconds}`;
+        },
+    },
+    methods: {
+        getRandomTime() {
+            const maxSeconds = 900;
+            return maxSeconds;
+        },
+        goToHome() {
+            this.$router.push('/');
+        },
+        startCountdown() {
+            this.timerInterval = setInterval(() => {
+                if (this.totalTime > 0) {
+                    this.totalTime -= 1;
+                    this.initialTime += 1;
+                } else {
+                    clearInterval(this.timerInterval);
+                }
+            }, 1000);
+        },
+    },
+    mounted() {
+        this.startCountdown();
+    },
+    beforeUnmount() {
+        clearInterval(this.timerInterval);
     },
 };
 </script>
 <style scoped>
 .timer-top {
-    height: 60vh;
+    height: 60%;
     display: flex;
     align-items: center;
 }
 .timer-bottom {
     background-color: white;
     padding: 0 !important;
-    height: 40vh;
+    height: 40%;
+}
+
+.formatted-time {
+    font-size: 4rem;
+    font-weight: 900;
+    padding-top: 1rem;
+    color: #ff66ba;
+}
+.back-home {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.back-home-text {
+    font-size: 1.25rem;
+    color: #66baff;
+    margin-left: 0.25rem;
 }
 </style>
